66881eb4b5872d8ff4eb0bcf80311c39b32323e2
Jonas Wessman Nielsen <5615825@RTS-365.dk>
2020-01-07 10:40:27 +0100

    jsdoc

---
+++ doc.js
@@ -0,0 +1,23 @@
+/**
+ * CommonJS / NodeFetch
+ * @type {Promise} 
+ */
+let myFetch = require("./umd");
+
+myFetch.init({
+    address: "https://pokeapi.co/api/v2/",
+    key: "1234"
+});
+
+myFetch.get("pokemon/ditto/").then(result => console.log(result));
+
+/**
+ * amd /XMLHttpRequest
+ */
+const XMLHttpRequest = "";
+
+
+/**
+ * Browser Response
+ */
+const Browser = "";
